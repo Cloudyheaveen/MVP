9f314fc47aecd43e3a1803801ecba121356f54e6
Cloudyheaveen <abirarahou2002@gmail.com>
2023-12-21 17:32:09 -0800

    ARAHOU ABIR

---
+++ README.md
@@ -0,0 +1,64 @@
+Build your portfolio project (Week 2): MVP Complete
+# Pet transportation -  <a href="https://prevozljubimaca.rs/">Live Preview</a> 
+
+<p>The application
+The concept of WessalPet was born out of the recognition
+of a common problem faced by pet owners:
+the challenge of finding pet-friendly transportation,
+especially without a personal vehicle.
+This idea expanded into creating a dedicated platform to bridge this gap.
+</p>
+
+### Key Features:
+- Mixins
+- Nesting
+- Partials
+- Maps
+- Variables
+- CSS Flexbox/Grid layout
+- Position layout
+- Animations
+- 3D Transformations
+- Semantic elements
+- 100% responsive
+- Cross browser compatibility
+- Navbar navigation
+- Functional hamburger menu
+- Admin panel
+- Functional contact form
+- Validate contact form
+- Reset button for contact form
+- Connection to the database
+- Enrollment in the database
+
+
+### Libraries & Plugins:
+- Font awesome
+- Google fonts
+
+## Geting started
+ This section contains how you can seting up this project locally. To get a local copy up and running follow these simple example steps.
+ 
+1. Clone the repo
+```
+git clone https://github.com/Cloudyheaveen/MVP.git
+```
+2. Run SASS
+```
+sass --watch assets/scss/style.scss:assets/css/style.css
+```
+
+## Contributing
+If oyu have some idea which would make better this project, __create a PR__. Don't forget to give the project a star! Thanks again!
+
+1. Fork the project
+2. Create and move on __Feature branch__ ```git checkout -b feature```
+3. Change the files you want ```vim <name of the file> ```
+4. Commit these changes ```git commit -m "Message" ```
+5. Push branch ```git push origin feature ```
+6. Open a pull request
+
+## Contact
+Email - <abirarahou2002@gmail.com> 
+
+Linkedin - <a href="https://www.linkedin.com/in/abir-arahou-6a6855270//">Linkedin profile</a>
---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WessalPet - prevoz ljubimaca</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/about-img.jpg" type="image/x-icon">
+
+    <!-- Meta tags -->
+    <meta name="author" content="Filip Zivanovic">
+    <meta name="tittle" content="WessalPet - prevoz ljubimaca">
+    <meta name="keywords" content="Prevoz. Ljubimci. Prevoz ljubimaca. Prevoz pasa. Prevoz macaka">
+    <meta name="description" content="Prevoz ljubimaca je savršen način da Vaš ljubimac stigne na
+            željeno mesto bez
+            stresa za Vas i njega. Zbog problema sa kojima se vlasnici kućnih ljubimaca svakodnevno susreću vezani za
+            prevoz, taxijem ili javnim prevozom, mi smo odlučili da Vam pomognemo.">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Google fonts link -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap"
+        rel="stylesheet">
+
+    <!-- Font awesome link -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- AOS CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+</head>
+
+<body>
+
+
+    <!-- Navbar section -->
+    <nav class="bg-linear w-100">
+        <div class="nav-content container">
+            <a href="index.php" class="logo fw-800"><strong>WessalPet</strong></a>
+            <ul class="fw-bold text-uppercase">
+                <li class="mr-3"><a href="index.php">Početna</a></li>
+                <li class="mr-3"><a href="#about">O nama</a></li>
+                <li class="mr-3"><a href="#services">Usluge</a></li>
+                <li class="mr-3"><a href="#contact">Kontakt</a></li>
+                <li class="mr-3"><a href="#gallery">Galerija</a></li>
+            </ul>
+            <div class="toggle"></div>
+        </div>
+
+        <div class="hamburger-menu text-center bg-linear hidden">
+            <ul class="fw-600 text-uppercase" id="list">
+                <li><a href="index.php">Početna</a></li>
+                <li class="pt-2"><a href="#about">O nama</a></li>
+                <li class="pt-2"><a href="#services">Usluge</a></li>
+                <li class="pt-2"><a href="#contact">Kontakt</a></li>
+                <li class="pt-2"><a href="#gallery">Galerija</a></li>
+            </ul>
+        </div>
+
+    </nav>
+    <!-- End navbar section -->
+
+
+
+
+    <!-- Header section -->
+    <header class="text-center">
+        <div class="header-content">
+            <h1 class="text-white fw-800 text-uppercase">Najudobniji prevoz Vaših ljubimaca</h1>
+            <button type="button">
+                <a href="tel:+381628143648">Pozovite sada</a>
+            </button>
+        </div>
+    </header>
+    <!-- Header banner section -->
+
+
+
+
+    <!-- About section -->
+    <section class="about container" id="about">
+        <h3 class="section-tittle" data-aos="fade-up">O nama</h3>
+        <h4 class="section-subtittle" data-aos="fade-up">Najpovoljniji prevoz Vaših ljubimaca - Zakažite i uverite se
+        </h4>
+        <p class="mt-2 text-justify" data-aos="fade-up"><strong>Prevoz ljubimaca</strong> je savršen način da Vaš
+            ljubimac stigne na
+            željeno mesto bez
+            stresa za Vas i njega. Zbog problema sa kojima se vlasnici kućnih ljubimaca svakodnevno susreću vezani za
+            prevoz, taxijem ili javnim prevozom, mi smo odlučili da Vam pomognemo. Tako što ćemo Vas i Vaše ljubimce
+            bezbedno odvesti do željene adrese. Takođe imamo mesta za putnike koji će biti u pratnji pasa, ali nije
+            obavezno jer naši vozači su već dobro obučeni za brigu životinja. Vršimo prevoz do bilo koje adrese u
+            Srbiji.
+        </p>
+
+        <div class="about-content mt-3">
+            <article data-aos="fade-up">
+                <ul class="mt-3">
+                    <li><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz do novih vlasnika </li>
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz na izložbe</li>
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz do salona lepote za pse
+                    </li>
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz na rođendane</li>
+
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz do veterinarskih
+                        ambulanti</li>
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz od/do aerodroma</li>
+
+                    <li class="pt-1"><i class="fas fa-angle-right mr-2 text-primary"></i>Prevoz na bilo koju lokaciju u
+                        u Srbiji</li>
+                </ul>
+                <div class="about-icons mt-3">
+                    <i class="fas fa-map-marker-alt mr-2 mt-2"></i>
+                    <span>Balkanska 4, Beograd</span><br>
+                    <a href="tel:+381628143648 "><i class="fas fa-phone mr-2 mt-2"></i></a>
+                    <span>+381 62 81 43 648</span><br>
+                    <i class="fas fa-clock mr-2 mt-2"></i>
+                    <span>Pon-Ned: 07:00-22:00, hitne voznje: 24/7</span>
+                </div>
+            </article>
+
+            <article data-aos="fade-up">
+                <img src="assets/img/about-img.jpg" alt="about-img">
+            </article>
+        </div>
+    </section>
+    <!-- End about section -->
+
+
+
+
+    <!-- Services section -->
+    <section class="services" id="services">
+        <h3 class="section-tittle container">Usluge</h3>
+        <h4 class=" section-subtittle container text-white">Kako funkcioniše zakazivanje</h4>
+
+        <div class="services-content text-center container">
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-phone-alt"></i>
+                <i class="fas fa-comments"></i>
+                <h5 class="text-white">Zakažite termin</h5>
+                <p class="text-white fw-600">Pozivom na broj telefona ili popunjavanjem forme na sajtu</p>
+            </article>
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-calendar-alt"></i>
+                <i class="fas fa-clock"></i>
+                <h5 class="text-white">Potvrda termina</h5>
+                <p class="text-white fw-600">Ako je termin slobodan dobijate obaveštenje</p>
+            </article>
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-car"></i>
+                <i class="fas fa-angle-double-right"></i>
+                <h5 class="text-white">Početak vožnje</h5>
+                <p class="text-white fw-600">Vozač dolazi na unapred udogovorenu adresu i vreme</p>
+            </article>
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-exclamation"></i>
+                <i class="fas fa-bone"></i>
+                <h5 class="text-white">Dolazak na adresu</h5>
+                <p class="text-white fw-600">Na brz i bezbedan način dolazite do krajnje lokacije</p>
+            </article>
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-angle-double-left"></i>
+                <i class="fas fa-exclamation"></i>
+                <h5 class="text-white">Zavrsetak vožnje</h5>
+                <p class="text-white fw-600">Povratak na početnu lokaciju Vašeg ljubimca</p>
+            </article>
+            <article class="mt-4" data-aos="fade-up">
+                <i class="fas fa-money-bill-wave"></i>
+                <i class="fas fa-smile"></i>
+                <h5 class="text-white">Naplata vožnje</h5>
+                <p class="text-white fw-600">Naplata se vrši na kraju vožnje</p>
+            </article>
+        </div>
+    </section>
+    <!-- End services section -->
+
+
+
+
+    <!-- Contact section -->
+    <section class="contact container" id="contact">
+        <h3 class="section-tittle">Zakazivanje vožnje</h3>
+        <h4 class="section-subtittle">Kako da stupite u kontakt sa nama</h4>
+
+        <div class="contact-content mt-4">
+            <form action="assets/php/reachout.php" method="POST" name="contact" data-aos="fade-up">
+
+                <div class="form-group w-100">
+                    <input name="name" type="text" placeholder="Vaše ime" required class="form-control w-100" id="name">
+                    <input name="email" type="email" placeholder="Email Adresa" required class="form-control w-100"
+                        id="email">
+                </div>
+
+                <div class="form-group w-100">
+                    <input name="date" type="text" placeholder="Datum polaska" onfocus="(this.type='date')" required
+                        class="form-control w-100" id="date">
+                    <input name="time" type="text" placeholder="Vreme polaska" onfocus="(this.type='time')" required
+                        class="form-control w-100" id="time">
+                </div>
+
+                <div class="form-group w-100">
+                    <input name="location_a" type="text" placeholder="Početna lokacija" required
+                        class="form-control w-100" id="location_a">
+                    <input name="location_b" type="text" placeholder="Krajnja lokacija" required
+                        class="form-control w-100" id="location_b">
+                </div>
+
+                <input name="contact_phone" type="number" placeholder="Broj telefona" required
+                    class="form-control w-100" id="phone">
+                <textarea name="comment" cols="30" rows="10" class="form-control w-100" placeholder="Napomena"
+                    id="comment"></textarea>
+
+                <input type="submit" value="Zakaži Voznju" class="sbm-btn">
+                <button type="button" id="reset-btn">Resetuj formu</button>
+            </form>
+
+            <div class="contact-info mt-4" data-aos="fade-up">
+                <div class="contact-info-item text-center mt-3">
+                    <i class="fas fa-map-marker-alt mb-2"></i>
+                    <p class="fw-bold">Adresa:</p>
+                    <p>Balkanska 4, Beograd</p>
+                </div>
+                <div class="contact-info-item text-center mt-3">
+                    <a href="tel:+381628143648 "><i class="fas fa-phone mb-2"></i></a>
+                    <p class="fw-bold">Telefon:</p>
+                    <p>+381 62 81 43 648</p>
+                </div>
+                <div class="contact-info-item text-center mt-3">
+                    <i class="fa fa-paper-plane mb-2"></i>
+                    <p class="fw-bold">Email:</p>
+                    <p>pettaxi@prevozljubimaca.rs
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End contact section -->
+
+
+
+
+    <!-- Gallery section -->
+    <section class="gallery container" data-aos="fade-up">
+        <h3 class="section-tittle">Galerija</h3>
+        <h4 class="section-subtittle">Galerija ljubimaca naših klijenata</h4>
+
+        <div class="gallery-content mt-4">
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-1.jpg" alt="gallery-img">
+            </div>
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-2.jpg" alt="gallery-img">
+            </div>
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-3.jpg" alt="gallery-img">
+            </div>
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-4.jpg" alt="gallery-img">
+            </div>
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-5.jpg" alt="gallery-img">
+            </div>
+            <div class="gallery-content-item w-100">
+                <img src="assets/img/gallery-6.jpg" alt="gallery-img">
+            </div>
+        </div>
+    </section>
+    <!-- End gallery section -->
+
+
+
+    <!-- Subscription section -->
+    <section class="subscription bg-linear">
+        <div class="subscription-content container">
+
+            <div class="subscription-content-card">
+                <a href="index.php" class="sub-logo fw-bold"><strong>PetTaxi</strong></a>
+                <p class="text-white">Best Pet Taxi</p>
+            </div>
+
+            <div class="subscription-content-card">
+                <h3 class="pb-2 text-white">Linkovi</h3>
+                <ul>
+                    <li><a href="index.php" class="text-white"><i
+                                class="fas fa-angle-right mr-2 text-white"></i>Početna</a>
+                    </li>
+                    <li class="pt-1"><a href="#about"><i class="fas fa-angle-right mr-2 text-white"></i>O nama</a>
+                    </li>
+                    <li class="pt-1"><a href="#services"><i class="fas fa-angle-right mr-2 text-white"></i>Usluge</a>
+                    </li>
+                    <li class="pt-1"><a href="#gallery"><i class="fas fa-angle-right mr-2 text-white"></i>Galerija</a>
+                    </li>
+                    <li class="pt-1"><a href="#contact"><i class="fas fa-angle-right mr-2 text-white"></i>Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="subscription-content-card">
+                <h3 class="pb-2 text-white">Radno vreme</h3>
+                <ul class="text-white">
+                    <li>Pon: 07:00 - 22:00</li>
+                    <li class="pt-1">Uto: 07:00 - 22:00</li>
+                    <li class="pt-1">Sre: 07:00 - 22:00</li>
+                    <li class="pt-1">Cet: 07:00 - 22:00</li>
+                    <li class="pt-1">Pet: 07:00 - 22:00</li>
+                    <li class="pt-1">Sub: 07:00 - 22:00</li>
+                    <li class="pt-1">Ned: 07:00 - 22:00</li>
+                </ul>
+            </div>
+
+            <div class="subscription-content-card">
+                <h3 class="pb-2 text-white">Kontakt</h3>
+                <ul class="text-white">
+                    <li>+381 62 81 43 648</li>
+                    <li class="pt-1">pettaxi@prevozljubimaca.rs
+                    </li>
+                    <li class="pt-1"> Balkanska 4</li>
+                    <li class="pt-1">Srbija, Beograd</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="subscription-copyright mt-4">
+            <p class="pt-4 text-center text-white">©2022 <span class="fw-bold">Filip Živanović</span>. All Right
+                Reserved</p>
+        </div>
+    </section>
+    <!-- End Subscription section -->
+
+
+    <!-- Custom JS -->
+    <script src="assets/js/main.js"></script>
+
+
+    <!-- AOS JS -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init(
+            {
+                duration: 800
+            }
+        );
+    </script>
+
+
+</body>
+
+</html>
